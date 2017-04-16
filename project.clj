f52ddf288bfe2ce1e8032105f61bbb3b862bbb64
Mike Hewitt <matroxmike@icloud.com>
2017-04-16 16:59:01 +1000

    version update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject systemProperties "1.0.1-SNAPSHOT"
+(defproject systemProperties "1.0.2-SNAPSHOT"
   :description "System Properties derived from Java"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   
