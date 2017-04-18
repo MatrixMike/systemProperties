437a28059bfd2aa26d77002968d5d6d5ad35bb02
MikeH <matroxmike@icloud.com>
2017-04-18 23:22:22 +1000

    AOT stuff

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject systemProperties "1.0.2-SNAPSHOT"
   :description "System Properties derived from Java"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  
+  :aot :all 
   :main systemProperties.core)
